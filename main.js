5bb20d09a39e558e3cf0296c8fb3957b0ff91852
Ethan Simpson <ethansown@gmail.com>
2018-11-12 22:02:41 -0600

    Init, Created Clock Face

---
+++ main.js
@@ -0,0 +1,55 @@
+function clock(){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    console.log(canvas.height);
+    var radius = canvas.height / 2;
+    console.log(radius);
+    ctx.translate(radius, radius);
+    radius *= 0.90;
+    console.log(radius);
+    drawClock();
+
+    function drawClock() {
+        drawFace(ctx, radius);
+        drawNumbers(ctx, radius);
+    }
+    function drawFace(ctx, radius){
+        var grad;
+
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, 2*Math.PI);
+        ctx.fillStyle = "white";
+        ctx.fill();
+
+        grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+        grad.addColorStop(0, "#333");
+        grad.addColorStop(0.5, "white");
+        grad.addColorStop(1.0, "#333");
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = radius * 0.1;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+        ctx.fillStyle = "#333";
+        ctx.fill();
+    }
+    function drawNumbers(ctx, radius){
+        var ang;
+        var num;
+        ctx.font = radius*0.15 + "px arial";
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        for(num = 1; num < 13; num++){
+            ang = num * Math.PI / 6;
+            ctx.rotate(ang);
+            ctx.translate(0, -radius * 0.85);
+            ctx.rotate(-ang);
+            ctx.fillText(num.toString(), 0, 0);
+            ctx.rotate(ang);
+            ctx.translate(0, radius * 0.85);
+            ctx.rotate(-ang);
+        }
+    }
+}
+window.addEventListener("load", clock);
