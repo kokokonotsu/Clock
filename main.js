126b4901d59db7cbc31655ca94b75fc473e0e613
Ethan Simpson <ethansown@gmail.com>
2018-11-12 22:16:31 -0600

    Finished Clock Face/Hands

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ function clock(){
     function drawClock() {
         drawFace(ctx, radius);
         drawNumbers(ctx, radius);
+        drawTime(ctx, radius);
     }
     function drawFace(ctx, radius){
         var grad;
@@ -50,6 +51,34 @@ function clock(){
             ctx.translate(0, radius * 0.85);
             ctx.rotate(-ang);
         }
+    }
+    function drawTime(ctx, radius){
+        var now = new Date();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var second = now.getSeconds();
+        //Hour Start
+        hour = hour % 12;
+        hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (6 * 360));
+        drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+        //Hour End
+        //Minute Start
+        minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+        drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+        //Minute End
+        //Second Start
+        second = (second * Math.PI / 30);
+        drawHand(ctx, second, radius * 0.9, radius * 0.02);
+    }
+    function drawHand(ctx, pos, length, width){
+        ctx.beginPath();
+        ctx.lineWidth = width;
+        ctx.lineCap = "round";
+        ctx.moveTo(0, 0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
     }
 }
 window.addEventListener("load", clock);
